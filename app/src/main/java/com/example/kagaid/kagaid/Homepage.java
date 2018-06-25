d616afa3201497b3c92f8326cc7c4de5bb61fa82
kentmicht <tkentmichaell@gmail.com>
2018-06-25 18:50:29 +0800

    Latest Version

---
+++ app/src/main/java/com/example/kagaid/kagaid/Homepage.java
@@ -0,0 +1,58 @@
+package com.example.kagaid.kagaid;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Homepage extends AppCompatActivity {
+
+    public Button logoutBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+        //initLogoutBtn();
+    }
+
+    public void logOut(View view) {
+        alertLogout();
+    }
+
+    public void alertLogout(){
+        AlertDialog.Builder a_builder = new AlertDialog.Builder(Homepage.this);
+        a_builder.setMessage("Do you really want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        openLogin();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = a_builder.create();
+        alert.setTitle("Kag-Aid");
+        alert.show();
+    }
+
+    public void openLogin(){
+        Intent login = new Intent(this, LogIn.class);
+        startActivity(login);
+    }
+
+    public void map(View view){
+        Intent mapNav = new Intent(this, MapsActivity.class);
+        startActivity(mapNav);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/kagaid/kagaid/LogIn.java
@@ -0,0 +1,56 @@
+package com.example.kagaid.kagaid;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LogIn extends AppCompatActivity {
+
+    //public Button loginBtn = (Button)findViewById(R.id.loginBtn);
+    EditText username;
+    EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+        //initLoginBtn();
+    }
+
+    public void goToHomepage(View view) {
+        username = (EditText) findViewById(R.id.loginUser);
+        password = (EditText) findViewById(R.id.loginPwd);
+        if(TextUtils.isEmpty(username.getText().toString()) == true || TextUtils.isEmpty(password.getText().toString()) == true){
+            Toast.makeText(LogIn.this, "You did not enter a username/password", Toast.LENGTH_LONG).show();
+        }else{
+            Intent homepage = new Intent(this, Homepage.class);
+            startActivity(homepage);
+        }
+
+    }
+
+
+//    public void initLoginBtn(){
+//        loginBtn.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+////                if(username.getText().toString().isEmpty() == true && password.getText().toString().isEmpty() == true){
+////                    Toast.makeText(LogIn.this, "You did not enter a username/password", Toast.LENGTH_LONG).show();
+//////                    //emptyText();
+////                }else{
+//                    startActivity(new Intent(LogIn.this, Homepage.class));
+////                }
+////
+//            }
+//        });
+
+
+
+}
